f30a73b21b8c836dc899cdeb13af759873956546
Darlan <darlan.sgf@gmail.com>
2019-03-25 17:50:09 -0300

    DB

--- bd/estoque.sql
+++ bd/estoque.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100138
  File Encoding         : 65001
 
- Date: 15/03/2019 17:15:13
+ Date: 25/03/2019 17:49:55
 */
 
 SET NAMES utf8mb4;
@@ -22,14 +22,14 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `produto`;
 CREATE TABLE `produto`  (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `fk_usuario` int(255) NOT NULL,
-  `nome_produto` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `quantidade` int(255) NULL DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `tipo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `fk_usuario` int(11) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `produto`(`fk_usuario`) USING BTREE,
-  CONSTRAINT `produto` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+  INDEX `produto_usuario`(`fk_usuario`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for usuario
@@ -38,16 +38,10 @@ DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario`  (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `sexo` int(255) NOT NULL,
-  `nascimento` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `rua` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `numero` int(255) NOT NULL,
-  `cidade` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `estado` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `cpf` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `cpf` int(255) NOT NULL,
   `senha` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
